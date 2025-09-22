ec7a9cb53a6e20fd40e3ff4fb4bfbf8357fd3458
Evan Silva <evanildosilva467@gmail.com>
2025-09-22 16:02:26 -0300

    Configurando a Stack

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,10 @@
-import { Slot } from "expo-router"
-import { View } from "react-native"
+import { Stack } from "expo-router"
 
 export default function Layout() {
     return (
-        <View style={{ flex: 1 }}>
-            <View style={{ width: "100%", height: 70, backgroundColor: "#000" }} />
-            <Slot />
-            <View style={{ width: "100%", height: 70, backgroundColor: "#C3C3c3" }} />
-        </View>
+        <Stack screenOptions={{headerShown: false}}>
+            <Stack.Screen name="index" options={{title: "Entrar"}}/>
+            <Stack.Screen name="sign-up" options={{title: "Criar Conta"}}/>
+        </Stack>
     )
 }
