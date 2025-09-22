e05c2eddb83c508420fdb9fbde94d8de7bfdc68e
Evan Silva <evanildosilva467@gmail.com>
2025-09-22 20:04:37 -0300

    Customizando Drawer

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,46 @@
+import { MaterialIcons } from "@expo/vector-icons"
 import { Drawer } from "expo-router/drawer"
 import { GestureHandlerRootView } from "react-native-gesture-handler"
 
 export default function Layout() {
     return (
-         <GestureHandlerRootView style={{flex: 1}}>
-            <Drawer />
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <Drawer
+                screenOptions={{
+                    drawerActiveTintColor: "#000",
+                    drawerInactiveTintColor: "#999"
+                }}
+            >
+                <Drawer.Screen
+                    name="index"
+                    options={{
+                        drawerLabel: "Entrar",
+                        title: "Entrar",
+                        drawerIcon: ({ color, size }) => (
+                            <MaterialIcons
+                                name="home" 
+                                size={size}
+                                color={color}
+                                />
+                        )
+                    }}
+                />
+                <Drawer.Screen
+                    name="sign-up"
+                    options={{
+                        drawerLabel: "Criar Conta",
+                        title: "Criar Conta",
+                        drawerIcon: ({ color, size }) => (
+                            <MaterialIcons
+                                name="add" 
+                                size={size}
+                                color={color}
+                                />
+                        )
+                    }}
+                />
+            </Drawer>
         </GestureHandlerRootView>
     )
-       
+
 }
