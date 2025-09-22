db7513d6342ff358f21e71e00ba4db7fd7ee5be2
Evan Silva <evanildosilva467@gmail.com>
2025-09-22 19:28:25 -0300

    Customizando Tabs

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,34 @@
+import { MaterialIcons } from "@expo/vector-icons"
 import { Tabs } from "expo-router"
 
 export default function Layout() {
     return (
-        <Tabs screenOptions={{ headerShown: false}}/>
+        <Tabs
+            screenOptions={{
+                headerShown: false,
+                tabBarLabelPosition: "beside-icon",
+                tabBarInactiveTintColor: "#CECECE",
+                tabBarActiveTintColor: "#000",
+            }}
+        >
+            <Tabs.Screen
+                name="Index"
+                options={{
+                    tabBarLabel: "Sign In",
+                    tabBarIcon: ({ color }) => (
+                        <MaterialIcons name="home" size={20} color={color} />
+                    )
+                }}
+            />
+            <Tabs.Screen
+                name="SignUp"
+                options={{
+                    tabBarLabel: "person",
+                    tabBarIcon: ({ color }) => (
+                        <MaterialIcons name="home" size={20} color={color} />
+                    )
+                }}
+            />
+        </Tabs>
     )
 }
