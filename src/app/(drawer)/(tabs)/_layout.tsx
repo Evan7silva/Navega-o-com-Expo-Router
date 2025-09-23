7a13f8f3f89e17ee7456aef7ebeb60ad30ce8ffc
Evan Silva <evanildosilva467@gmail.com>
2025-09-22 23:20:40 -0300

    Configurando Tabs

--- src/app/(drawer)/(tabs)/_layout.tsx
+++ src/app/(drawer)/(tabs)/_layout.tsx
@@ -1,5 +1,39 @@
+import { MaterialIcons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
-
 export default function TabsLayout() {
-    return <Tabs/>
+    return (
+        <Tabs
+            screenOptions={{
+                headerShown: false,
+                tabBarLabelPosition: "beside-icon",
+            }}>
+            <Tabs.Screen
+                name="index"
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialIcons
+                            name="list"
+                            color={color}
+                            size={size}
+                        />
+                    ),
+                    tabBarLabel: "Produtos"
+                }}
+            />
+
+            <Tabs.Screen
+                name="order"
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialIcons
+                            name="shopping-bag"
+                            color={color}
+                            size={size}
+                        />
+                    ),
+                    tabBarLabel: "Pedido"
+                }}
+            />
+        </Tabs>
+    )
 }
